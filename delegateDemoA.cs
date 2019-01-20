81f91b5d09eb251929e499828412349e559eb7db
yaoshun35 <yaoshun35@qq.com>
2019-01-21 00:43:27 +0800

    上传了脚本
    AB配合 。 实现鼠标左右点击，出现不同事件的简单演示

---
+++ delegateDemoA.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class delegateDemoA : MonoBehaviour {
+
+
+    //声明委托类型，以及对应的事件
+    public delegate void DoSomething();
+    public event DoSomething UserMouseLeftDown;
+    public event DoSomething UserMouseRightDown;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //左右鼠标按下，触发这个事件，但具体做什么，由脚本B为事件添加具体功能。
+
+
+        if (Input.GetMouseButton(0)) {
+
+
+            UserMouseLeftDown();
+
+        }
+
+        if (Input.GetMouseButton(1)) {
+
+
+            UserMouseRightDown();
+        }
+
+
+
+	}
+
+
+
+
+}
---
+++ delegateUseB.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class delegateUseB : MonoBehaviour {
+
+
+    //拿到脚本A
+    public delegateDemoA da;
+
+
+
+    //装载，卸载脚本A上的事件。
+    //在这里定义具体的办法。
+    private void OnEnable()
+    {
+        da.UserMouseLeftDown += left;
+        da.UserMouseRightDown += Right;
+    }
+
+
+    private void OnDisable()
+    {
+        da.UserMouseLeftDown -= left;
+        da.UserMouseRightDown -= Right;
+    }
+
+
+
+    public void left() {
+
+        Debug.Log("left!");
+    }
+
+    public void Right()
+    {
+
+        Debug.Log("Right!");
+    }
+
+}
